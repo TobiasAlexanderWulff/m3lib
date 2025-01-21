5960e431dc05225bd96e0232511dd5f0198a9d49
Tobias Wulff <tobias.wulff@live.de>
2025-01-21 15:09:04 +0100

    add trapezverfahren

---
+++ m3lib/numerik/trapezverfahren.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def trapezverfahren(f, a, b, n):
+    """
+    Berechnet das bestimmte Integral von f(x) von a bis b
+    
+    Parameter:
+    f : function
+        Integrand
+    a : float
+        Untere Grenze
+    b : float
+        Obere Grenze
+    n : int
+        Anzahl der Teilintervalle/Säulen (Stützstellen - 1)
+    
+    Rückgabe:
+    float
+        Näherung des bestimmten Integrals
+    """
+    h = (b - a) / n # Schrittweite
+    x = np.linspace(a, b, n + 1) # Stützstellen
+    y = f(x) 
+    return h * (y[0] / 2 + np.sum(y[1:-1]) + y[-1] / 2)
