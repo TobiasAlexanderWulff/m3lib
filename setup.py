6af440913de66b89c1a1f0dd63c567a8431c8c49
Tobias Wulff <tobias.wulff@live.de>
2025-01-21 15:08:56 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='m3lib',
+    version='0.1',
+    description='Implementation of various mathematical functions',
+    license='MIT',
+    packages=['m3lib'],
+    author='Tobias Alexander Wulff',
+    author_email='tobias.wulff@live.de',
+    keywords=['math', 'functions', 'implementation', 'numerical', 'algorithms', 'complex', 'numbers'],
+    url='https://github.com/tobiasalexanderwulff/m3lib',
+)
