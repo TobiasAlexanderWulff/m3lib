362584a146df6f1ec4ed6a70876a102a4745bc8a
Tobias Wulff <tobias.wulff@live.de>
2025-01-21 15:53:41 +0100

    add simpsonverfahren

---
+++ m3lib/numerik/simpsonverfahren.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def simpsonverfahren(f, a, b, n):
+    """
+    Berechnet das bestimmte Integral von f(x) von a bis b
+    
+    Parameter:
+    f : function
+        Integrand
+    a : float
+        Untere Grenze
+    b : float
+        Obere Grenze
+    n : int
+        Anzahl der Teilintervalle/Doppelsäulen (Stützstellen - 1)
+    
+    Rückgabe:
+    float
+        Näherung des bestimmten Integrals
+    """
+    h = (b - a) / 2 * n # Schrittweite (2n wegen Doppelsäulen)
+    x = np.linspace(a, b, n + 1) # Stützstellen
+    y = f(x) 
+    return (h / 3) * (y[0] + y[-1] + 2 * np.sum(y[2:-1:2]) + 4 * np.sum(y[1:-1:2]))
