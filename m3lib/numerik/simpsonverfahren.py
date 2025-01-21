d8bce9bcd032a0b7b335c346c53b65e553d75c3e
Tobias Wulff <tobias.wulff@live.de>
2025-01-21 17:19:25 +0100

    add fehlerberechnung simpson

--- m3lib/numerik/simpsonverfahren.py
+++ m3lib/numerik/simpsonverfahren.py
@@ -23,3 +23,26 @@ def simpsonverfahren(f, a, b, n):
     x = np.linspace(a, b, 2 * n + 1) # Stützstellen
     y = f(x) 
     return (h / 3) * (y[0] + y[-1] + 2 * np.sum(y[2:-2:2]) + 4 * np.sum(y[1:-1:2]))
+
+
+def max_fehler_simpsonverfahren(f4, a, b, n):
+    """
+    Berechnet den maximalen Fehler des Simpsonverfahrens
+    
+    Parameter:
+    f4 : function 
+        Vierte Ableitung des Integranden
+    a : float
+        Untere Grenze
+    b : float
+        Obere Grenze
+    n : int
+        Anzahl der Teilintervalle/Doppelsäulen (Stützstellen - 1)
+        
+    Rückgabe:
+    float
+        Fehler des Simpsonverfahrens
+    """
+    h = (b - a) / (2 * n) # Schrittweite (2n wegen Doppelsäulen)
+    x = np.linspace(a, b, 2 * n + 1) # Stützstellen
+    return ((b - a) / 180) * h**4 * np.sum(f4(x))
