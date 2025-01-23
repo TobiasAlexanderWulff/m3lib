f3baef0bbb280884f0e5515ad0a1445f72aae544
Tobias Wulff <tobias.wulff@live.de>
2025-01-23 14:38:28 +0100

    add fehler trapezverfahren

--- m3lib/numerik/trapezverfahren.py
+++ m3lib/numerik/trapezverfahren.py
@@ -23,3 +23,25 @@ def trapezverfahren(f, a, b, n):
     x = np.linspace(a, b, n + 1) # Stützstellen
     y = f(x) 
     return (h / 2) * (y[0] + y[-1] + 2 * np.sum(y[1:-1]))
+
+def max_feher_trapezverfahren(f2, a, b, n):
+    """
+    Berechnet den maximalen Fehler des Trapezverfahrens
+    
+    Parameter:
+    f2 : function 
+        Zweite Ableitung des Integranden
+    a : float
+        Untere Grenze
+    b : float
+        Obere Grenze
+    n : int
+        Anzahl der Teilintervalle/Säulen (Stützstellen - 1)
+        
+    Rückgabe:
+    float
+        Fehler des Trapezverfahrens
+    """
+    h = (b - a) / n # Schrittweite
+    x = np.linspace(a, b, n + 1) # Stützstellen
+    return ((b - a) / 12) * h**2 * np.max(np.abs(f2(x)))
